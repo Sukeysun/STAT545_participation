016f86c9d8f297f1ddaeb62fad48800ef2996f63
Sukeysun <578108683@qq.com>
2018-11-13 09:52:10 -0800

    rename 105 and upload 107

---
+++ 105 106/powers.Rcheck/00_pkg_src/powers/R/cube.R
@@ -0,0 +1,6 @@
+#' @title Cube the input
+#' @param x Vector of numerics
+#' @return The vecotr x, cubed.
+#' @rdname common_doc
+#' @export
+cube <- function(x) pow(x,3)
---
+++ 105 106/powers_0.1.0/powers/R/square.R
@@ -0,0 +1,5 @@
+#' @title Sqaure the input
+#' @param x Vector of numerics
+#' @return The vecotr x, squared.
+#' @export
+square <- function(x) x^2
